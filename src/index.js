09385c5174422734832a536279553dfb816e12ce
kafkaesc <jared.a.hettinger@gmail.com>
2023-01-12 17:06:04 -0600

    Spaces to tabs. Other prettier options set too.

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import About from "pages/About";
-import App from "App";
-import City from "pages/City";
-import Error from "Error";
-import Home from "pages/Home";
-import reportWebVitals from "./reportWebVitals";
+import About from 'pages/About';
+import App from 'App';
+import City from 'pages/City';
+import Error from 'Error';
+import Home from 'pages/Home';
+import reportWebVitals from './reportWebVitals';
 
-import "./index.css";
+import './index.css';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/About", element: <About /> },
-      { path: "/City/:cityCode", element: <City /> },
-      { path: "/Home", element: <Home /> },
-    ],
-  },
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <Error />,
+		children: [
+			{ path: '/', element: <Home /> },
+			{ path: '/About', element: <About /> },
+			{ path: '/City/:cityCode', element: <City /> },
+			{ path: '/Home', element: <Home /> },
+		],
+	},
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,31 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function Home() {
-  return (
-    <div className="content-column text-center">
-      <h2>Home</h2>
-      <p>A React site about the dream of rail in Texas.</p>
-      <p>
-        <Link to="/City/DAL">Dallas</Link>
-        {" // "}
-        <Link to="/City/AUS">Austin</Link>
-        {" // "}
-        <Link to="/City/SAN">San Antonio</Link>
-        {" // "}
-        <Link to="/City/HOU">Houston</Link>
-        {" // "}
-        <Link to="/City/ELP">El Paso</Link>
-      </p>
-      <p>
-        <Link to="/About">About</Link>
-      </p>
-      <img
-        alt="A black and white scene of a train arriving into a station from the French film The Arrival of a Train at La Ciotat Station."
-        className="App-logo"
-        src={require("assets/images/larrivee-dun-train.jpg")}
-      />
-    </div>
-  );
+	return (
+		<div className="content-column text-center">
+			<h2>Home</h2>
+			<p>A React site about the dream of rail in Texas.</p>
+			<p>
+				<Link to="/City/DAL">Dallas</Link>
+				{' // '}
+				<Link to="/City/AUS">Austin</Link>
+				{' // '}
+				<Link to="/City/SAN">San Antonio</Link>
+				{' // '}
+				<Link to="/City/HOU">Houston</Link>
+				{' // '}
+				<Link to="/City/ELP">El Paso</Link>
+			</p>
+			<p>
+				<Link to="/About">About</Link>
+			</p>
+			<img
+				alt="A black and white scene of a train arriving into a station from the French film The Arrival of a Train at La Ciotat Station."
+				className="App-logo"
+				src={require('assets/images/larrivee-dun-train.jpg')}
+			/>
+		</div>
+	);
 }
 
 export default Home;
