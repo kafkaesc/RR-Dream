a6a6b2adcb59c7ca1ec50770353515ddc26f25cd
kafkaesc <jared.a.hettinger@gmail.com>
2023-01-24 22:04:26 -0600

    Removing unnecessary MemoryRouter.

--- src/components/SocialLinks.test.js
+++ src/components/SocialLinks.test.js
@@ -1,21 +1,20 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 import SocialLinks from './SocialLinks';
 
 test('Has GitHub Link', () => {
-	render(<SocialLinks />, { wrapper: MemoryRouter });
+	render(<SocialLinks />);
 	const githubLinkElement = screen.getByAltText('GitHub');
 	expect(githubLinkElement).toBeInTheDocument();
 });
 
 test('Has Twitter Link', () => {
-	render(<SocialLinks />, { wrapper: MemoryRouter });
+	render(<SocialLinks />);
 	const githubLinkElement = screen.getByAltText('Twitter');
 	expect(githubLinkElement).toBeInTheDocument();
 });
 
 test('Has Instagram Link', () => {
-	render(<SocialLinks />, { wrapper: MemoryRouter });
+	render(<SocialLinks />);
 	const githubLinkElement = screen.getByAltText('Instagram');
 	expect(githubLinkElement).toBeInTheDocument();
 });
