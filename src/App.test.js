c641317dff7d9ece6af942e2786df1553588d1f2
kafkaesc <jared.a.hettinger@gmail.com>
2023-02-05 09:13:39 -0600

    Updating app test with MemoryRouter wrapper

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 it('Has the Main Title', () => {
-	render(<App />);
+	render(<App />, { wrapper: MemoryRouter });
 	const h1Title = screen.getByRole('heading', { level: 1 });
 	expect(h1Title).toBeInTheDocument();
 	expect(h1Title).toHaveTextContent(/texas rail/i);
