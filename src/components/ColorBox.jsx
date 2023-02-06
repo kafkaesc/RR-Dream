b2615008eeb3c8573e967ac9bbd91479b57d9440
kafkaesc <jared.a.hettinger@gmail.com>
2023-02-06 08:31:33 -0600

    Declaring class outside of return to trigger Tailwind consistently.

--- src/components/ColorBox.jsx
+++ src/components/ColorBox.jsx
@@ -1,7 +1,11 @@
 function ColorBox({ boxColor }) {
+	// Concatenating the bg-* within className will cause
+	// Tailwind to not compile the bg class unless they're
+	// use elsewhere in the app.
+	const bg = 'bg-' + boxColor;
 	return (
 		<li className="inline-block mx-1 border border-rail-black">
-			<div className={`w-16 h-16 sm:w-24 sm:h-24 bg-${boxColor}`}></div>
+			<div className={`w-16 h-16 sm:w-24 sm:h-24 ${bg}`}></div>
 		</li>
 	);
 }
